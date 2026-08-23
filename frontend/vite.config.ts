57db28c90839470a38e897ba144c1c43d94e287d
NITHINRAJR-2020 <inthinrajkv@gmail.com>
2026-08-23 15:45:23 +0530

    frontend working 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,19 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    proxy: {
-      "/api": "http://localhost:8000",
-      "/market": "http://localhost:8000",
-      "/health": "http://localhost:8000",
+  proxy: {
+    "/api": {
+      target: "https://apimarket-mp7s.onrender.com",
+      changeOrigin: true,
     },
+    "/market": {
+      target: "https://apimarket-mp7s.onrender.com",
+      changeOrigin: true,
+    },
+    "/health": {
+      target: "https://apimarket-mp7s.onrender.com",
+      changeOrigin: true,
+    },
+  },
   },
 });
